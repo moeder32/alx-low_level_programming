119f5440f07671500aa9bf83f694de088107a1a4
moeder32 <kgomotso.mangoegape@yahoo.co.za>
2022-12-10 02:14:30 -0800

    three alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,20 +6,22 @@
  */
 int main(void)
 {
-       	char c = 'a';
-
-        while (c <= 'z')
-	{                	
-	        putchar(c);
-                c++;       
+	char c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
 	}
-        c = 'A';
-
-        while (c <= 'Z')
-	{	
-                putchar(c);
-                c++;
-        }
+	
+	c = 'A';
+	
+	while (c <= 'Z')
+	
+	{
+		putchar(c);
+		c++;
+	}
+	
 	putchar('\n');
 	return (0);
 }
