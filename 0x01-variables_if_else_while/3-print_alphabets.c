055bc81f3e3188c773dc5b9b34da9e7185f96f5f
moeder32 <kgomotso.mangoegape@yahoo.co.za>
2022-12-09 17:35:57 -0800

    three alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,6 +7,7 @@
 int main(void)
 {
         char c;
+
         char d;
 
         c = 'a';
@@ -20,7 +21,7 @@ int main(void)
                 (d <= 'z') {
                         putchar(d);
                         d++;
-                }
+		}
         putchar('\n');
         return (0);
 }
