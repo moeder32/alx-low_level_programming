8c6f83123764e2fc1e0df61954e9518baf52b755
moeder32 <kgomotso.mangoegape@yahoo.co.za>
2022-12-11 07:22:21 -0800

    eight print

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
