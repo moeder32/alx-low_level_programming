c95e96be1ac15c642644f3b752ea0116ce69bf9d
moeder32 <kgomotso.mangoegape@yahoo.co.za>
2022-12-14 11:27:22 -0800

    seven last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,12 +6,10 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n % 10;
-
-	if (ld < 0)
-		ld *= -1;
-
-	_putchar(ld + '0');
-
+	print_last_digit(98);
+	print_last_digit(0);
+	n = print_last_digit(1024);
+	_putchar('0' + n);
+	_putchar('\n');
 	return (0);
 }
