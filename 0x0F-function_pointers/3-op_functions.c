9172972b05e51dfdd587a002c8773088aadc6803
moeder32 <kgomotso.mangoegape@yahoo.co.za>
2023-01-20 10:49:05 -0800

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -39,6 +39,17 @@ int op_mul(int a, int b)
 return (a * b);
 }
 /**
+ * op_div - Returns the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The quotient of a and b.
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
  * op_mod - Returns the remainder of the division of two numbers.
  * @a: The first number.
  * @b: The second number.
