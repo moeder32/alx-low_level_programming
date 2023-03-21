1c508f82ecf21ae25714817e2ca4a6d18938d70a
moeder32 <kgomotso.mangoegape@yahoo.co.za>
2023-03-21 03:46:26 -0700

    task 0 to task 1

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,10 @@
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    printf("%d\n", _strlen("My Dyn Lib"));
+    return (EXIT_SUCCESS);
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  _abs - that computes the absolute of an integer
+ *  @f: The integer to be completed
+ *  Return: The absolute value
+ */
+int _abs(int f)
+{
+	if (f >= 0)
+		return (f);
+	else
+		return (-f);
+}
