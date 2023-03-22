2b6c296dc5a7ac05aa565f005644c6ee3d345f37
moeder32 <kgomotso.mangoegape@yahoo.co.za>
2023-03-22 06:56:32 -0700

    task 1

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
  * main - check the code
  *
- * Return: EXIT_SUCCESS.
+ * Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
     printf("%d\n", _strlen("My Dyn Lib"));
-    return (EXIT_SUCCESS);
+    return (1);
 }
